c3a79004f15dd7978eb8059b7cefe0ab608c4807
pkathuri <pkathuri19@gmail.com>
2024-08-04 18:19:49 +0300

    second commit

--- MySQLServer.py
+++ MySQLServer.py
@@ -12,11 +12,11 @@ def create_database(database_name,host,user,password):
         db_creation_query = "CREATE DATABASE IF NOT EXISTS alx_book_store"
         cursor.execute(db_creation_query)
         print('alx_book_store created successfully!')
-    except Error as e:
+    except mysql.connector.Error as e:
         print(e)
     finally:
         if connection is not None:
-            print("Database created successfully.")
+            connection.close()
 
 if __name__ == "__main__":
     create_database('alx_book_store','localhost','root','kiama1996')
